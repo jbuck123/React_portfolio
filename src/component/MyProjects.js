1ac3f2872acbe0c3d507cf3201775eccd5bb3a88
JamesB <James.buchmann@gmail.com>
2022-08-24 09:58:26 -0500

    added grid layout projects

--- src/component/MyProjects.js
+++ src/component/MyProjects.js
@@ -2,10 +2,25 @@ import React from 'react'
 
 export default function MyProjects() {
     return (
-        <div>
-            <p className='.bodyComponent'>
-                Projects
-            </p>
+      <div className="parent">
+        <div className="children">
+          <h1>Item 1</h1>
         </div>
-    )
+        <div className="children">
+          <h1>Item 2</h1>
+        </div>
+        <div className="children">
+          <h1>Item 3</h1>
+        </div>
+        <div className="children">
+          <h1>Item 4</h1>
+        </div>
+        <div className="children">
+          <h1>Item 5</h1>
+        </div>
+        <div className="children">
+          <h1>Item 6</h1>
+        </div>
+      </div>
+    );
 } 
