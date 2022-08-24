2f231d840b6c0c838d153f9191428982c81a4684
JamesB <James.buchmann@gmail.com>
2022-08-24 18:05:07 -0500

    Contact Me page completed
    need to fix bug to stop multiple components from toggling

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function Contact() {
     return (
-        <div>
-       <h3></h3>
-        </div>
-    )
+      <div className='container'>
+        <form>
+          <label for="fname">First Name</label>
+          <input type={"text"} id="fname" name="firstname"></input>
+
+          <label for="lname">Last Name</label>
+          <input type={"text"} id="lname" name="lastname"></input>
+
+          <label for="subject">Leave a Message</label>
+          <textarea id='subject' name="subject" placeholder='Write something... ' style={{height:"200px"}}></textarea>
+          <input type={"submit"} value="Submit"></input>
+        </form>
+      </div>
+    );
 } 
