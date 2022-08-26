895d22b6c86a693f482a28af21b19bdb8b7b487d
JamesB <James.buchmann@gmail.com>
2022-08-26 13:21:17 -0500

    Adding to the content of the  About me

--- src/component/AboutMe.js
+++ src/component/AboutMe.js
@@ -4,7 +4,11 @@ export default function AboutMe() {
     return (
         <div>
            <h1>About Me</h1>
-           
+           <p>
+               Hello! I am new to web development and coding in general. I was a carpenter with a passion for tech, building computers and videogames.
+               currently I am trying to switch my hobby into my career and my former career as a carpenter into a hobby! I am a graduate from a coding bootcamp hosted by the 
+               University of Minnesota. I am working hard to develop new skills in React, Javascript, css, html and incorporate other awesome frameworks! 
+           </p>
            <img className='aboutMePicture' src='images/About_me.jpeg' alt='Blue Ribbon Pines Disc Golf Course'></img>
         </div>
     )
