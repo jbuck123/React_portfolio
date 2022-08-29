aebff19024129ee2574d1dd15e94bd321e1bd780
JamesB <James.buchmann@gmail.com>
2022-08-29 09:49:57 -0500

    updates on the projects component

--- src/component/MyProjects.js
+++ src/component/MyProjects.js
@@ -5,9 +5,12 @@ export default function MyProjects() {
       <div className="parent">
         <div className="children">
           <h1>Item 1</h1>
+    
         </div>
         <div className="children">
           <h1>Item 2</h1>
+          <a href="https://minotaurius.github.io/Certified_Tomatoes/"><img className='scheduler' src="images/workDayScheduler.png"></img></a>
+
         </div>
         <div className="children">
           <h1>Item 3</h1>
@@ -17,6 +20,9 @@ export default function MyProjects() {
         </div>
         <div className="children">
           <h1>Item 5</h1>
+          <p>Certified Tomatoes was my First project at the U of M bootcamp.</p>
+          <p>This basic web application is a movie guessing game that utilizes IMDB api!</p>
+          <a href="https://minotaurius.github.io/Certified_Tomatoes/"><img src="images/project_demo.gif"></img></a>
         </div>
         <div className="children">
           <h1>Item 6</h1>
