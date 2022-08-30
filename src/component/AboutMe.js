d79b1eb7726b70fe8a40aae99100f19daae58980
JamesB <James.buchmann@gmail.com>
2022-08-30 09:27:52 -0500

    worked on about me page

--- src/component/AboutMe.js
+++ src/component/AboutMe.js
@@ -2,14 +2,18 @@ import React from 'react'
 
 export default function AboutMe() {
     return (
-        <div>
-           <h1>About Me</h1>
-           <p>
-               Hello! I am new to web development and coding in general. I was a carpenter with a passion for tech, building computers and videogames.
+        <div className='aboutmeContainer'>
+           <h1 className='name'>James Buchmann</h1>
+           <h2 className='title'>UI / UX Designer and Developer</h2>
+           <div className='aboutImage'>
+               <img src="https://picsum.photos/200/300?grayscale" alt="photo of me"></img>
+           </div>
+           <p className='aboutMeText'>
+               Hello! I am new to web development and coding in general. I was a carpenter with a passion for tech dating back to early childhood.
                currently I am trying to switch my hobby into my career and my former career as a carpenter into a hobby! I am a graduate from a coding bootcamp hosted by the 
-               University of Minnesota. I am working hard to develop new skills in React, Javascript, css, html and incorporate other awesome frameworks! 
+               University of Minnesota. I am working hard to develop new skills in React, Javascript, CSS, and HTML. 
            </p>
-           <img className='aboutMePicture' src='images/About_me.jpeg' alt='Blue Ribbon Pines Disc Golf Course'></img>
+           
         </div>
     )
 } 
