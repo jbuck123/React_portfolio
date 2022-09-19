1f0be470f760290d7c0abfce985f867028be58e3
JamesB <James.buchmann@gmail.com>
2022-09-19 11:55:31 -0500

    Routing complete

--- src/App.js
+++ src/App.js
@@ -1,90 +1,34 @@
 // import logo from './logo.svg';
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, {useState} from 'react';
-import MyProjects from "./component/MyProjects";
+// import { Outlet,  } from "react-router-dom"
 
-import AboutMe from './component/AboutMe';
-import Resume from "./component/Resume";
+// import MyProjects from "./Pages/MyProjects";
+import { Routes, Route} from "react-router-dom"
+import AboutMe from "./Pages/AboutMe"
+import Header from './component/Header'
+import MyProjects from "./Pages/MyProjects";
+import Resume from "./Pages/Resume"
 
-import './App.css';
+
 
 
 // look into using buttons instead of anchor tags
 
-
+// instead of loadin
 
 
 function App() {
-  const homePage = (
-    <div>
-      <h1>About Me</h1>
-
-      <img
-        className="aboutMePicture"
-        src="images/About_me.jpeg"
-        alt="Blue Ribbon Pines Disc Golf Course"
-      ></img>
-    </div>
-  );
-  const [page, setPage] = useState(homePage);
-  const handleClick = (event) => {
-    setPage(event.target.dataset.page);
-    console.log(event.target.dataset);
-  };
-
   return (
     <div>
-      <header className="header">
-        <h1>James Buchmann</h1>
-        <a onClick={handleClick} data-page="aboutMe">
-          About Me
-        </a>
-        <a onClick={handleClick} data-page="projects">
-          My Projects
-        </a>
-
-        <a onClick={handleClick} data-page="resume">
-          Resume
-        </a>
-      </header>
-      
-      <body className="bodyComponents">
-        {/*show component on click */}
-        {page === "aboutMe" && <AboutMe />}
-        {page === "projects" && <MyProjects />}
-       
-        {page === "resume" && <Resume />}
-      </body>
-      <footer>
-        <div className="wrapper">
-          <div className="links">
-            <a href="google.com">
-              <img
-                src="Logos/link.png"
-                alt="Linkiden logo"
-                className="logo"
-              ></img>
-            </a>
-            <a href="google.com">
-              <img
-                src="Logos/GitHub_Logo.png"
-                alt="github logo"
-                className="logo"
-              ></img>
-            </a>
-            <a href="google.com">
-              <img
-                src="Logos/gmail.png"
-                alt="gmail logo"
-                className="logo"
-              ></img>
-            </a>
-          </div>
-        </div>
-        <div className="footer-bottom"></div>
-      </footer>
+    <Header />
+        <Routes>
+        <Route path="/" element={<AboutMe ></AboutMe>}></Route>
+        <Route path="/Projects" element={<MyProjects />}></Route>
+        <Route path="/Resume" element={<Resume />}></Route>
+        </Routes>
+    
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+// import logo from "../images/GAMRx10.png";
+
+function Header(props) {
+  return (
+    <header >
+      {props.user ? (
+        <nav id="navStyle">
+          {/* <img id="logoPic" src={logo} alt="logo" /> */}
+          <span className="centerText">Welcome, {props.user.email}
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/logout">Log Out</NavLink>
+          </span>
+        </nav>
+      ) : (
+        <nav id="navStyle">
+          {/* <img id="logoPic" src={logo} alt="logo" /> */}
+          <div className="centerText">
+          <NavLink to="/">Home</NavLink> | {""}
+          <NavLink to="/Projects">Projects</NavLink> | {""}
+          <NavLink to="/Resume">Resume</NavLink>
+          </div>
+        </nav>
+      )}
+    </header>
+  );
+}
+
+export default Header;
