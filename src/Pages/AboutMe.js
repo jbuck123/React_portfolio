41700a93f3f3623887a9d29760496bc06189daf5
JamesB <James.buchmann@gmail.com>
2022-09-21 16:28:57 -0500

    changes to the project page

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -1,55 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function AboutMe() {
-    return (
-      <div className='about-me-content'>
+  return (
+    <div className="about-me-content">
       <div className="aboutmeContainer-left">
         <h1 className="name">James Buchmann</h1>
         <h2 className="title">Web Developer</h2>
-      <div className='about-me-photo'>
-      <img
-            src="./images/Aboutme.jpeg"
-            alt="photo of me"
-            
-            ></img>
-      </div>
-      <p className='photo-context'>(Happy guy in the white shirt)</p>  
-     
-      
+        <div className="about-me-photo">
+          <img src="./images/Aboutme.jpeg" alt="photo of me"></img>
+        <p className="photo-context">(Happy guy in the white shirt)</p>
+        </div>
         <p className="aboutMeText">
-          Hello! I am new to web development and coding in general. I was a
-          carpenter with a passion for tech dating back to early childhood.
-          currently I am trying to switch my hobby into my career and my former
-          career as a carpenter into a hobby! I am a graduate from a coding
-          bootcamp hosted by the University of Minnesota. I am working hard to
-          develop new skills in React, Javascript, CSS, and HTML.
+          Aspiring web-developer who loves React, UI / UX Design. and JavaScript. Creating websites is my passion, with passion comes dedication, to be dedicated requires a form of habit. 
         </p>
-        
+
       </div>
       <div className="aboutmeContainer-right">
-        <h1 className="name">James Buchmann</h1>
-        <h2 className="title">Web Developer</h2>
-      <div className='about-me-photo'>
-      <img
-            src="./images/Aboutme.jpeg"
-            alt="photo of me"
-            
-            ></img>
+        <div className="right-content">
+      <p className="right-body-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+  
+    
+
       </div>
-      <p className='photo-context'>(Happy guy in the white shirt)</p>  
-     
-      
-        <p className="aboutMeText">
-          Hello! I am new to web development and coding in general. I was a
-          carpenter with a passion for tech dating back to early childhood.
-          currently I am trying to switch my hobby into my career and my former
-          career as a carpenter into a hobby! I am a graduate from a coding
-          bootcamp hosted by the University of Minnesota. I am working hard to
-          develop new skills in React, Javascript, CSS, and HTML.
-        </p>
-        
-      </div>
-      </div>
-      
-    );
-} 
+    </div>
+  );
+}
--- src/Pages/MyProjects.js
+++ src/Pages/MyProjects.js
@@ -1,32 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function MyProjects() {
-    return (
-      <div className="parent">
-        <div className="children">
-          <h1>Item 1</h1>
-    
-        </div>
-        <div className="children">
-          <h1>Scheduler</h1>
-          <a href="https://github.com/jbuck123/Scheduler"><img className='project-image' src="images/workDayScheduler.png"></img></a>
-
-        </div>
-        <div className="children">
-          <h1>Item 3</h1>
-        </div>
-        <div className="children">
-          <h1>Item 4</h1>
-        </div>
-        <div className="children">
-          <h1>Certified Tomatoes</h1>
-          <p>Certified Tomatoes was my First project at the U of M bootcamp.</p>
-          <p>This basic web application is a movie guessing game that utilizes IMDB api!</p>
-          <a href="https://github.com/Minotaurius/Certified_Tomatoes"><img src="images/project_demo.gif" className='project-image'></img></a>
-        </div>
-        <div className="children">
-          <h1>Item 6</h1>
-        </div>
+  return (
+    <div className="parent">
+      <div className="child">
+        <img src="./images/certified-tomatoes.gif"></img>
       </div>
-    );
-} 
+      <div className="child">
+        <img src="./images/noteTaker.png"></img>
+      </div>
+      <div className="child">
+        <img src = "./images/scheduler-project.png"></img>
+      </div>
+      <div className="child">4</div>
+    </div>
+  );
+}
