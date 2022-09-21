9ecade969583d3abba4ef74729d111706f42cda3
JamesB <James.buchmann@gmail.com>
2022-09-21 10:57:50 -0500

    css AboutMe styling

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function AboutMe() {
     return (
-      <div className="aboutmeContainer">
+      <div className='about-me-content'>
+      <div className="aboutmeContainer-left">
         <h1 className="name">James Buchmann</h1>
         <h2 className="title">Web Developer</h2>
       <div className='about-me-photo'>
@@ -23,6 +24,32 @@ export default function AboutMe() {
           bootcamp hosted by the University of Minnesota. I am working hard to
           develop new skills in React, Javascript, CSS, and HTML.
         </p>
+        
       </div>
+      <div className="aboutmeContainer-right">
+        <h1 className="name">James Buchmann</h1>
+        <h2 className="title">Web Developer</h2>
+      <div className='about-me-photo'>
+      <img
+            src="./images/Aboutme.jpeg"
+            alt="photo of me"
+            
+            ></img>
+      </div>
+      <p className='photo-context'>(Happy guy in the white shirt)</p>  
+     
+      
+        <p className="aboutMeText">
+          Hello! I am new to web development and coding in general. I was a
+          carpenter with a passion for tech dating back to early childhood.
+          currently I am trying to switch my hobby into my career and my former
+          career as a carpenter into a hobby! I am a graduate from a coding
+          bootcamp hosted by the University of Minnesota. I am working hard to
+          develop new skills in React, Javascript, CSS, and HTML.
+        </p>
+        
+      </div>
+      </div>
+      
     );
 } 
