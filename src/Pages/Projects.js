54d2a4967a74fdb829e49ac7de02fcdc4eaba36f
JamesB <James.buchmann@gmail.com>
2022-09-23 16:29:39 -0500

    added projects to the Project Page. Created helper and component for Project page.

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ProjectItem from "../component/ProjectItem" 
-import Tomatoes from '../assets/certified-tomatoes.gif'
+import Tomatoes from '../assets/tomatoes.gif'
 // import imgs
 
-import "../styles/Projects.css"
+import "../styles/Project.css"
 
 export default function Projects() {
   return (
     <div className="projects">
       <h1> My Personal Projects</h1>
-      <div className="prjectList">
+      <div className="projectList">
+        <ProjectItem name = "Certified Tomatoes" image={Tomatoes} />
+        <ProjectItem name = "Certified Tomatoes" image={Tomatoes} />
+        <ProjectItem name = "Certified Tomatoes" image={Tomatoes} />
         <ProjectItem name = "Certified Tomatoes" image={Tomatoes} />
       </div>
     </div>
---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,25 @@
+import gamr from "../assets/Gamr.png"
+import noteTaker from "../assets/noteTaker.png"
+import tomatoes from "../assets/tomatoes"
+import scheduler from "../assets/scheudler"
+
+
+
+export const ProjectList = [
+    {
+        name: "Gamr" ,
+        image: gamr,
+    },
+    {
+        name: "Note Taker App" ,
+        image: noteTaker,
+    },
+    {
+        name: "Certified Tomatoes" ,
+        image: tomatoes,
+    },
+    {
+        name:  "Work Day Scheduler" ,
+        image: scheduler,
+    },
+]
