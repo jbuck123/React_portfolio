1e846aa11732c3c335dfc88d882acb85e8d4a316
JamesB <James.buchmann@gmail.com>
2022-10-26 16:47:20 -0500

    home section styled

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,22 +5,30 @@ import Profile from "../assets/profile-pic.png";
 
 // mui imports
 import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 export default function Home() {
   return (
     <div className="child-container">
-      <Avatar
-        alt="Remy Sharp"
-        variant="square"
-        sx={{ width: 120, height: 120 }}
-        src={Profile}
-      />
-      <h1>hello world</h1>
-      <h3>I am James Buchmann</h3>
-      <h6> I have: </h6>
-      <p> A passion for creating websites</p>
-      <p> A strong desire to learn</p>
-      <p> A beautiful backhand drive</p>
+      <div className="home-padding">
+        <Avatar
+          alt="Remy Sharp"
+          variant="square"
+          sx={{ width: 120, height: 120 }}
+          src={Profile}
+        />
+        <h1 className="home-title">hello world</h1>
+        <h1 className="home-title">I am James Buchmann</h1>
+        <div className="home-links">
+          <h3 className="home-subtitle">Important Links</h3>
+          <Stack spacing={2} direction="row">
+            <Button target='_blank' href="https://github.com/jbuck123" variant="outlined">GitHub</Button>
+            <Button target='_blank' href="https://www.linkedin.com/in/james-buchmann-b82b76225/" variant="outlined">LinkedIn</Button>
+            <Button target='_blank' href="https://github.com/jbuck123" variant="outlined">Blog</Button>
+          </Stack>
+        </div>
+      </div>
     </div>
   );
 }
